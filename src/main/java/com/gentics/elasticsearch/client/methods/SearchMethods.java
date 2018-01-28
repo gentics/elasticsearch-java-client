9e7766ac9d7ee559325a675c4262495d5b41ff7d
Johannes Schüth <git@jotschi.de>
2018-01-28 14:46:27 +0100

    Add scroll methods

--- src/main/java/com/gentics/elasticsearch/client/methods/SearchMethods.java
+++ src/main/java/com/gentics/elasticsearch/client/methods/SearchMethods.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import com.gentics.elasticsearch.client.HttpErrorException;
 
 import io.reactivex.Single;
+import io.vertx.core.json.JsonObject;
 
 /**
  * Search API related methods.
@@ -14,16 +15,24 @@ import io.reactivex.Single;
  */
 public interface SearchMethods<T> extends HTTPMethods<T> {
 
-	default T search(T query, String... indices) throws HttpErrorException {
+	default T query(T query, String... indices) throws HttpErrorException {
 		String indicesStr = StringUtils.join(indices, ",");
 		String path = indicesStr + "/_search";
 		return get(path, query);
 	}
 
-	default Single<T> searchAsync(T query, String... indices) {
+	default Single<T> queryAsync(T query, String... indices) {
 		String indicesStr = StringUtils.join(indices, ",");
 		String path = indicesStr + "/_search";
 		return getAsync(path, query);
+	}
+
+	default T queryScroll(T request) throws HttpErrorException {
+		return post("_search", request);
+	}
+
+	default Single<T> queryScrollAsync(T request) {
+		return postAsync("_search", request);
 	}
 
 	// TODO add scroll support https://www.elastic.co/guide/en/elasticsearch/reference/current/search-request-scroll.html
