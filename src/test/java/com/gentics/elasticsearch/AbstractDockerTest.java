b9eda3bd2f962734105f5283390a3c524dde8e14
Serhii Plyhun <S.Plyhun@gentics.com>
2024-06-19 16:50:29 +0200

    GPU-1602: No more deprecated logging

--- src/test/java/com/gentics/elasticsearch/AbstractDockerTest.java
+++ src/test/java/com/gentics/elasticsearch/AbstractDockerTest.java
@@ -7,15 +7,8 @@ import com.gentics.elasticsearch.client.okhttp.ElasticsearchOkClient;
 import com.gentics.elasticsearch.docker.ElasticsearchContainer;
 
 import io.vertx.core.json.JsonObject;
-import io.vertx.core.logging.LoggerFactory;
-import io.vertx.core.logging.SLF4JLogDelegateFactory;
 
 public class AbstractDockerTest {
-
-	static {
-		// Use slf4j instead of JUL
-		System.setProperty(LoggerFactory.LOGGER_DELEGATE_FACTORY_CLASS_NAME, SLF4JLogDelegateFactory.class.getName());
-	}
 
 	@ClassRule
 	public static ElasticsearchContainer elasticsearch = new ElasticsearchContainer(true);
